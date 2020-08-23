4ed1e5476fcac3c55256a5d60219505278863216
ItMustWorked <marta.obolentseva@yandex.ru>
2020-08-24 01:15:06 +0300

    final ps calculation puted in separate module

---
+++ src/modules/final_ps.py
@@ -0,0 +1,17 @@
+from initial_parameters import DataFiles
+from power_spectrum import Data
+
+def final_ps(filename_config):
+
+    files = DataFiles()
+    files.read_input(filename_config)
+    files.info()
+    data = Data()
+
+    #read data from files
+    data.read_raw_data_from(files.data)
+    data.define_freq_bounds()
+    data.rmbg()
+    data.find_final_ps()
+    data.save_to(files.data)
+    print('all data saved')
--- src/tests/test_final_ps.py
+++ src/tests/test_final_ps.py
@@ -1,28 +1,5 @@
 import sys
 sys.path.insert(0, "C:\\Users\\Marta\\source\\repos\\crunchy-taiyaki\\Speckle\\src\\modules")
-from initial_parameters import DataFiles
-from power_spectrum import Data
+from final_ps import final_ps
 
-files = DataFiles()
-files.read_input('C:\\Users\\Marta\\source\\repos\\crunchy-taiyaki\\Speckle\\src\\inputs\\TEST_star_input.txt')
-files.info()
-data = Data()
-
-##calc all data
-#print('data calculation...')
-#data.dark = middle_dark(files.dark,files.dark_frames)
-#data.ref_dark = middle_dark(files.ref_dark,files.ref_dark_frames)
-#data.flat = middle_flat(files.flat,files.flat_frames)
-#data.star_ps.values = obj_ps(files.star,files.star_frames, data.dark, data.flat)
-#data.ref_ps.values = obj_ps(files.ref,files.ref_frames, data.ref_dark, data.flat)
-#print('all done!')
-#data.save_to(files.data)
-#print('data saved')
-
-#read data from files
-data.read_raw_data_from(files.data)
-data.define_freq_bounds()
-data.rmbg()
-data.find_final_ps()
-data.save_to(files.data)
-print('all data saved')
+final_ps('C:\\Users\\Marta\\source\\repos\\crunchy-taiyaki\\Speckle\\src\\inputs\\TEST_star_input.txt')
