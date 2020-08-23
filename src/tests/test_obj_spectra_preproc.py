95740875e458c840a8b12cca3c6f3ebbb620e5de
ItMustWorked <marta.obolentseva@yandex.ru>
2020-08-24 01:11:29 +0300

    spectra preprocessing take out into separate module

---
+++ src/tests/test_obj_spectra_preproc.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.insert(0, "C:\\Users\\Marta\\source\\repos\\crunchy-taiyaki\\Speckle\\src\\modules")
+from obj_spectra_preproc import objects_spectra_preproc
+
+objects_spectra_preproc('C:\\Users\\Marta\\source\\repos\\crunchy-taiyaki\\Speckle\\src\\inputs\\TEST_star_input.txt')
