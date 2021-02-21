959ca32d6406b47e47f100168dffd447f9fcbe83
Marta <marta.obolentseva@yandex.ru>
2021-02-21 15:11:58 +0300

    added model residuals diagrams

--- src/modules/file_reader.py
+++ src/modules/file_reader.py
@@ -132,6 +132,12 @@ class FitParametersReader:
         self.up_freq_border = None
         self.bandwidth = None # ширина шага по частоте
 
+    def to_float(self,value):
+        if value is not None:
+            return float(value)
+        else:
+            return None
+
     def read(self,file):
         info = []
         with open(file, 'r') as input:
@@ -140,30 +146,31 @@ class FitParametersReader:
                 if text.startswith('#'):
                     continue
                 info.append(text)
+        info = [x if x!='-' else None for x in info] # заменяем '-' на None
         self.star_type = info[0]
         self.rmbg_flag = info[1]
         self.zone_type = info[2]
         self.method = info[3]
-        self.dm21 = float(info[4])
-        self.dm21_bottom = float(info[5])
-        self.dm21_upper = float(info[6])
-        self.x2 = float(info[7])
-        self.x2_bottom = float(info[8])
-        self.x2_upper = float(info[9])
-        self.y2 = float(info[10])
-        self.y2_bottom = float(info[11])
-        self.y2_upper = float(info[12])
+        self.dm21 = self.to_float(info[4])
+        self.dm21_bottom = self.to_float(info[5])
+        self.dm21_upper = self.to_float(info[6])
+        self.x2 = self.to_float(info[7])
+        self.x2_bottom = self.to_float(info[8])
+        self.x2_upper = self.to_float(info[9])
+        self.y2 = self.to_float(info[10])
+        self.y2_bottom = self.to_float(info[11])
+        self.y2_upper = self.to_float(info[12])
 
         if self.star_type == 'triple':
-            self.dm31 = float(info[13])
-            self.dm31_bottom = float(info[14])
-            self.dm31_upper = float(info[15])
-            self.x3 = float(info[16])
-            self.x3_bottom = float(info[17])
-            self.x3_upper = float(info[18])
-            self.y3 = float(info[19])
-            self.y3_bottom = float(info[20])
-            self.y3_upper = float(info[21])
+            self.dm31 = self.to_float(info[13])
+            self.dm31_bottom = self.to_float(info[14])
+            self.dm31_upper = self.to_float(info[15])
+            self.x3 = self.to_float(info[16])
+            self.x3_bottom = self.to_float(info[17])
+            self.x3_upper = self.to_float(info[18])
+            self.y3 = self.to_float(info[19])
+            self.y3_bottom = self.to_float(info[20])
+            self.y3_upper = self.to_float(info[21])
 
         self.b_freq_border = int(info[22])
         self.up_freq_border = int(info[23])
--- src/modules/plot_model.py
+++ src/modules/plot_model.py
@@ -42,275 +42,74 @@ angle_config = 'C:\\Users\\Marta\\source\\repos\\crunchy-taiyaki\\Speckle\\src\\
 #fit_parameters_config = 'C:\\Users\\Marta\\source\\repos\\crunchy-taiyaki\\Speckle\\src\\inputs\\TYC\\700_20200511_no_ref\\fit_parameters.txt'
 #angle_config = 'C:\\Users\\Marta\\source\\repos\\crunchy-taiyaki\\Speckle\\src\\inputs\\TYC\\700_20200511_no_ref\\angle.txt'
 
+def residuals_by_ring(ps, model, fit_result):
+    u,v = Grid(size).uv_meshgrid()
+    model_frame = np.empty((size,size))
+    f_ar_lenght = len(fit_result.f_ar)
+    for i in range(f_ar_lenght-1):
+        if (input_fit_parameters.star_type == 'triple'):
+            i_model = model(u,v,fit_result.I1_ar[i],\
+                        fit_result.dm21_ar[i],fit_result.x2_ar[i],fit_result.y2_ar[i],\
+                        fit_result.dm31_ar[i],fit_result.x3_ar[i],fit_result.y3_ar[i])
+        else:
+            i_model = model(u,v,fit_result.I1_ar[i],\
+                        fit_result.dm21_ar[i],fit_result.x2_ar[i],fit_result.y2_ar[i])
+        mask = ring_logical_mask(size, fit_result.f_ar[i],fit_result.f_ar[i+1])
+        model_frame = np.where(mask, model_frame, i_model)
+    return ps - model_frame
+
+def residuals(ps, model,I1_ar,f_ar, final_parameters):
+    u,v = Grid(size).uv_meshgrid()
+    model_frame = np.empty((size,size))
+    f_ar_lenght = len(f_ar)
+    for i in range(f_ar_lenght-1):
+        if (input_fit_parameters.star_type == 'triple'):
+            i_model = model(u,v,I1_ar[i],\
+                        final_parameters.dm21,final_parameters.x2,final_parameters.y2,\
+                        final_parameters.dm31,final_parameters.x3,final_parameters.y3)
+        else:
+            i_model = model(u,v,I1_ar[i],\
+                        final_parameters.dm21,final_parameters.x2,final_parameters.y2)
+        mask = ring_logical_mask(size, f_ar[i], f_ar[i+1])
+        model_frame = np.where(mask, model_frame, i_model)
+    return ps - model_frame
 
 files = FileInfoReader()
 files.read(filename_config)
 
 data = Data()
 data.read_from(files.data)
+size = data.star_ps.values.shape[0]
+half_size = size//2
+ps = data.rmbg_final_ps.values
 
 input_fit_parameters = FitParametersReader()
 input_fit_parameters.read(fit_parameters_config)
 
-#read i xy dm from files
+if (input_fit_parameters.star_type == 'triple'):
+    model = Models.triple
+else:
+    model = Models.binary
+
+
 fit_result = FitResult(filename_config,fit_parameters_config)
 fit_result.read(files.data)
 
+ring_residual = residuals_by_ring(ps, model, fit_result)
+plt.figure()
+plt.imshow(ring_residual)
+plt.colorbar()
+plt.show()
+
 sample = ResultSample(input_fit_parameters.star_type)
 sample.read_from(files.data)
-
 final_parameters = final_result(filename_config,fit_parameters_config,angle_config,'180')
-size = data.star_ps.values.shape[0]
-half_size = size//2
-u,v = Grid(size).uv_meshgrid()
 
-#create zones
-f_ar_lenght = len(fit_result.f_ar)
-sample_f_ar_lenght = len(sample.f)
-zones = np.empty((f_ar_lenght,size,size))
-ps = data.rmbg_final_ps.values
-model_data = np.empty_like(ps)
-clean_model_data = np.empty_like(ps)
-
-
-freq_axis = np.arange(-half_size,half_size)
-polar_residuals = np.empty((size,size))
-i_residuals = np.empty((size,size))
-
-
-if (input_fit_parameters.star_type == 'triple'):
-    model = Models.triple
-    # model
-    plt.figure()
-    for i in range(f_ar_lenght):
-        mask = ring_logical_mask(size,fit_result.f_ar[i],fit_result.f_ar[i]+input_fit_parameters.bandwidth)
-        model_data[mask] = model(u,v,fit_result.I1_ar[i],\
-                                    fit_result.dm21_ar[i],fit_result.x2_ar[i],fit_result.y2_ar[i],\
-                                    fit_result.dm31_ar[i],fit_result.x3_ar[i],fit_result.y3_ar[i])[mask]
-    plot_rings_borders(0,0,input_fit_parameters.mask_b_freq_border,input_fit_parameters.mask_up_freq_border)
-    plt.imshow(ring_mask(model_data,fit_result.f_ar[0],fit_result.f_ar[-11]), cmap='gray', extent=[-half_size,half_size,-half_size,half_size])
-    plt.title('model')
-    plt.colorbar()
-    plt.savefig(files.images + '\\model_ps.png')
-
-    #residuals
-    plt.figure()
-    for i in range(f_ar_lenght):
-        mask = ring_logical_mask(size,fit_result.f_ar[i],fit_result.f_ar[i]+input_fit_parameters.bandwidth)
-        model_data[mask] = model(u,v,fit_result.I1_ar[i],\
-                                    fit_result.dm21_ar[i],fit_result.x2_ar[i],fit_result.y2_ar[i],\
-                                    fit_result.dm31_ar[i],fit_result.x3_ar[i],fit_result.y3_ar[i])[mask]
-        values = np.sqrt((model_data[mask]-ps[mask])**2/ps[mask]**2)
-        residuals[mask] = values
-    plot_rings_borders(0,0,input_fit_parameters.mask_b_freq_border,input_fit_parameters.mask_up_freq_border)
-    plt.imshow(ring_mask(residuals,fit_result.f_ar[0],fit_result.f_ar[-11]), cmap='gray', extent=[-half_size,half_size,-half_size,half_size]\
-                                                 ,vmin=0,vmax=0.5)
-    plt.colorbar()
-    plt.savefig(files.images + '\\residuals2d.png')
-
-    plt.figure()
-    for i in range(sample_f_ar_lenght):
-        mask = ring_logical_mask(size,sample.f[i],sample.f[i]+input_fit_parameters.bandwidth)
-        clean_model_data[mask] = model(u,v,sample.I1[i],\
-                                    final_parameters.dm21,final_parameters.x2,final_parameters.y2,\
-                                    final_parameters.dm31,final_parameters.x3,final_parameters.y3)[mask]
-    plt.imshow(ring_mask(clean_model_data,sample.f[0],sample.f[-1]), cmap='gray', extent=[-half_size,half_size,-half_size,half_size])
-    plt.colorbar()
-    plt.savefig(files.images + '\\clean_model_ps.png')
-
-
-    #clean residuals
-    plt.figure()
-    for i in range(sample_f_ar_lenght):
-        mask = ring_logical_mask(size,sample.f[i],sample.f[i]+input_fit_parameters.bandwidth)
-        clean_model_data[mask] = model(u,v,sample.I1[i],\
-                                    final_parameters.dm21,final_parameters.x2,final_parameters.y2,\
-                                    final_parameters.dm31,final_parameters.x3,final_parameters.y3)[mask]
-        values = np.sqrt((clean_model_data[mask]-ps[mask])**2/ps[mask]**2)
-        residuals[mask] = values
-    plt.imshow(ring_mask(residuals,sample.f[0],sample.f[-1]), cmap='gray',extent=[-half_size,half_size,-half_size,half_size],vmin=0,vmax=0.5)
-    plt.colorbar()
-    plt.savefig(files.images + '\\clean_residuals2d.png')
-
-
-    plt.figure()
-    plt.plot(ring_mask(model(u,v,1.0, final_parameters.dm21,final_parameters.x2,final_parameters.y2,\
-                                    final_parameters.dm31,final_parameters.x3,final_parameters.y3),sample.f[0],sample.f[-1])[:,half_size],label='model')
-    plt.plot(ring_mask(ps,sample.f[0],sample.f[-1])[:,half_size],label='ps')
-    plt.legend()
-    plt.show()
-
-else:
-    model = Models.binary
-    if np.logical_not(np.all(np.isnan(data.ref_ps.values))):
-        plt.figure()
-        for i in range(f_ar_lenght):
-            clean_model_data = model(u,v,fit_result.I1_ar[i],\
-                                        fit_result.dm21_ar[i],fit_result.x2_ar[i],fit_result.y2_ar[i])
-            plt.plot(freq_axis,ring_mask(clean_model_data,\
-                fit_result.f_ar[i],fit_result.f_ar[i]+input_fit_parameters.bandwidth)[half_size,:],color='darkorange',label='model')
-        plt.plot(freq_axis,ring_mask(ps,fit_result.f_ar[0],fit_result.f_ar[-1])[half_size,:],color='black',alpha=0.6,label='ps')
-        plt.title('model_ps x')
-        plt.savefig(files.images + '\\model_ps_x.png')
-
-        plt.figure()
-        for i in range(f_ar_lenght):
-            clean_model_data = model(u,v,fit_result.I1_ar[i],\
-                                        fit_result.dm21_ar[i],fit_result.x2_ar[i],fit_result.y2_ar[i])
-            plt.plot(freq_axis,ring_mask(clean_model_data,\
-                fit_result.f_ar[i],fit_result.f_ar[i]+input_fit_parameters.bandwidth)[:,half_size],color='darkorange',label='model')
-        plt.plot(freq_axis,ring_mask(ps,fit_result.f_ar[0],fit_result.f_ar[-1])[:,half_size],color='black',alpha=0.6,label='ps')
-        plt.title('model_ps y')
-        plt.savefig(files.images + '\\model_ps_y.png')
-
-        plt.figure()
-        for i in range(sample_f_ar_lenght):
-            clean_model_data = model(u,v,sample.I1[i],\
-                                        final_parameters.dm21,final_parameters.x2,final_parameters.y2)
-            plt.plot(freq_axis,ring_mask(clean_model_data,sample.f[i],sample.f[i]+input_fit_parameters.bandwidth)[half_size,:],\
-                                         color='darkorange',label='model')
-        plt.plot(freq_axis,ring_mask(ps,sample.f[0],sample.f[-1])[half_size,:],color='black',alpha=0.6,label='ps')
-        plt.title('clean model_ps x')
-        plt.savefig(files.images + '\\clean_model_ps_x.png')
-
-        plt.figure()
-        for i in range(sample_f_ar_lenght):
-            clean_model_data = model(u,v,sample.I1[i],\
-                                        final_parameters.dm21,final_parameters.x2,final_parameters.y2)
-            plt.plot(freq_axis,ring_mask(clean_model_data,sample.f[i],sample.f[i]+input_fit_parameters.bandwidth)[:,half_size],\
-                                      color='darkorange',label='model')
-        plt.plot(freq_axis,ring_mask(ps,sample.f[0],sample.f[-1])[:,half_size],color='black',alpha=0.6,label='ps')
-        plt.title('clean model_ps y')
-        plt.savefig(files.images + '\\clean_model_ps_y.png')
-    else:
-        #plt.figure()
-        #for i in range(f_ar_lenght):
-        #    clean_model_data = model(u,v,fit_result.I1_ar[i],\
-        #                                fit_result.dm21_ar[i],fit_result.x2_ar[i],fit_result.y2_ar[i])
-        #    plt.plot(freq_axis,ring_mask(clean_model_data,\
-        #        fit_result.f_ar[i],fit_result.f_ar[i]+input_fit_parameters.bandwidth)[half_size,:],color='darkorange',label='model')
-        #plt.plot(freq_axis,ring_mask(ps,fit_result.f_ar[0],fit_result.f_ar[-1])[half_size,:],color='black',alpha=0.6,label='ps')
-        #plt.yscale('log')
-        #plt.title('model_ps x')
-        #plt.savefig(files.images + '\\model_ps_x.png')
-
-        #plt.figure()
-        #for i in range(f_ar_lenght):
-        #    clean_model_data = model(u,v,fit_result.I1_ar[i],\
-        #                                fit_result.dm21_ar[i],fit_result.x2_ar[i],fit_result.y2_ar[i])
-        #    plt.plot(freq_axis,ring_mask(clean_model_data,\
-        #        fit_result.f_ar[i],fit_result.f_ar[i]+input_fit_parameters.bandwidth)[:,half_size],color='darkorange',label='model')
-        #plt.plot(freq_axis,ring_mask(ps,fit_result.f_ar[0],fit_result.f_ar[-1])[:,half_size],color='black',alpha=0.6,label='ps')
-        #plt.yscale('log')
-        #plt.title('model_ps y')
-        #plt.savefig(files.images + '\\model_ps_y.png')
-
-        #fig = plt.figure()
-        #ax = fig.add_subplot(121)
-        #for i in range(f_ar_lenght):
-        #    residuals = ps - model(u,v,fit_result.I1_ar[i],\
-        #                                fit_result.dm21_ar[i],fit_result.x2_ar[i],fit_result.y2_ar[i])
-        #    #plt.imshow(ring_mask(residuals,\
-        #    #    fit_result.f_ar[i],fit_result.f_ar[i]+input_fit_parameters.bandwidth), cmap='gray', extent=[-256.0,256.0,256.0,-256.0])
-        #    i_residuals, i_angs = to_polar(residuals,[0.,0.],512*np.sqrt(2))
-        #    polar_residuals[radius_idx(fit_result.f_ar[i]),:] = i_residuals[radius_idx(fit_result.f_ar[i]),:]
-        #np.save(files.data + '\\residuals.npy',polar_residuals)
-        #plot = ax.imshow(polar_residuals, cmap='gray')
-        #ax.set_xticks(range(0, polar_residuals.shape[1]+1, 50))
-        #ax.set_xticklabels((i_angs[::50]*180/np.pi).round().astype(int))
-        #ax.set_title('residuals: ps-model')
-        ##fig.colorbar(plot)
-        #fig.savefig(files.images + '\\residuals.png')
-
-        #plt.figure()
-        #for i in range(f_ar_lenght):
-        #    clean_model_data = model(u,v,fit_result.I1_ar[i],\
-        #                                final_parameters.dm21,final_parameters.x2,final_parameters.y2)
-        #    plt.plot(freq_axis,ring_mask(clean_model_data,\
-        #                                 fit_result.f_ar[i],fit_result.f_ar[i]+input_fit_parameters.bandwidth)[half_size,:],\
-        #              color='red',label='model')
-        #    clean_model_data = model(u,v,fit_result.I1_ar[i],\
-        #                                0.2,final_parameters.x2,final_parameters.y2)
-        #    plt.plot(freq_axis,ring_mask(clean_model_data,\
-        #                                 fit_result.f_ar[i],fit_result.f_ar[i]+input_fit_parameters.bandwidth)[half_size,:],\
-        #              color='blue',label='dm=0.2')
-        #plt.plot(freq_axis,ring_mask(ps,fit_result.f_ar[0],fit_result.f_ar[-1])[half_size,:],color='black',alpha=0.6,label='ps')
-        #plt.yscale('log')
-        #plt.title('clean model_ps x')
-        #plt.savefig(files.images + '\\clean_model_ps_x.png')
-
-        #plt.figure()
-        #for i in range(f_ar_lenght):
-        #    clean_model_data = model(u,v,fit_result.I1_ar[i],\
-        #                                final_parameters.dm21,final_parameters.x2,final_parameters.y2)
-        #    plt.plot(freq_axis,ring_mask(clean_model_data,\
-        #                                 fit_result.f_ar[i],fit_result.f_ar[i]+input_fit_parameters.bandwidth)[:,half_size],\
-        #                                    color='red',label='model')
-        #    clean_model_data = model(u,v,fit_result.I1_ar[i],\
-        #                                0.2,final_parameters.x2,final_parameters.y2)
-        #    plt.plot(freq_axis,ring_mask(clean_model_data,\
-        #                                 fit_result.f_ar[i],fit_result.f_ar[i]+input_fit_parameters.bandwidth)[:,half_size],\
-        #                                    color='blue',label='dm=0.2')
-        #plt.plot(freq_axis,ring_mask(ps,fit_result.f_ar[0],fit_result.f_ar[-1])[:,half_size],color='black',alpha=0.6,label='ps')
-        #plt.yscale('log')
-        #plt.title('clean model_ps y')
-        #plt.savefig(files.images + '\\clean_model_ps_y.png')
-
-
-        def residuals_bound(freq):
-            idx = np.where(fit_result.f_ar == freq)
-            return ps - model(u,v,fit_result.I1_ar[idx],\
-                                        fit_result.dm21_ar[idx],\
-                                        fit_result.x2_ar[idx],fit_result.y2_ar[idx])
-
-        def model_i(freq):
-            idx = np.where(fit_result.f_ar == freq)
-            return model(u,v,fit_result.I1_ar[idx],\
-                                        fit_result.dm21_ar[idx],\
-                                        fit_result.x2_ar[idx],fit_result.y2_ar[idx])
-        def slice(image, freq):
-                data = np.empty((512,512))
-                for i in range(512):
-                    for j in range(512):
-                        if abs(((i-256)**2 + (j-256)**2) - freq**2) < 10**2:
-                            data[i,j] = image[i,j]
-                return data
-
-        #image_polar = np.empty((512,512))
-        #for freq in fit_result.f_ar:
-        #    res_bound = residuals_bound(freq)
-        #    data = slice(res_bound,freq)
-        #    polar_test, angs = to_polar(data,[256.,256.],256*np.sqrt(2))
-        #    image_polar[int(freq*np.sqrt(2)),:] = polar_test[int(freq*np.sqrt(2)),:]
-        #fig,ax = plt.subplots(1,1,figsize=(6,8))
-        #ax.imshow(image_polar,cmap='gray')
-        #ax.set_xticks(range(0, polar_test.shape[1]+1, 50))
-        #ax.set_xticklabels((angs[::50]*180/np.pi).round().astype(int))
-
-        #freq = 118.
-        #res_bound = residuals_bound(freq)
-        #data = slice(res_bound,freq)
-        #polar_test, angs = to_polar(data,[256.,256.],256*np.sqrt(2))
-        #fig,ax = plt.subplots(1,1)
-        #ax.plot(polar_test[int(freq*np.sqrt(2)),:])
-        #ax.set_xticks(range(0, polar_test.shape[1]+1, 50))
-        #ax.set_xticklabels((angs[::50]*180/np.pi).round().astype(int))  
-        #plt.title('ps-model' + ' freq=' + str(freq))
-
-        freq = 78.
-        model_data = slice(model_i(freq),freq)
-        ps_data = slice(ps,freq)
-        polar_ps, angs = to_polar(ps_data,[256.,256.],256*np.sqrt(2))
-        polar_model, angs = to_polar(model_data,[256.,256.],256*np.sqrt(2))
-        fig,ax = plt.subplots(1,1)
-        ax.plot(polar_ps[int(freq*np.sqrt(2)),:], label='ps')
-        ax.plot(polar_model[int(freq*np.sqrt(2)),:], label='model')
-        ax.set_xticks(range(0, 512, 50))
-        ax.set_xticklabels((angs[::50]*180/np.pi).round().astype(int)) 
-        plt.legend()
-        plt.title('ps-model' + ' freq=' + str(freq))
-
-    plt.show()
+residuals_data = residuals(ps, model, fit_result.I1_ar, fit_result.f_ar, final_parameters)
+plt.figure()
+plt.imshow(residuals_data)
+plt.colorbar()
+plt.show()
 
 
 
